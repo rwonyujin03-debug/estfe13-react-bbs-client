8402ef46aa3f46cc8fd160a6e29c626071484217
rwonyujin03-debug <rwonyujin03@gmail.com>
2026-07-02 11:53:23 +0900

    first commit

---
+++ src/components/Write.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import BoardList from "./components/BoardList";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>React BBS</h1>
+      <BoardList />
+    </div>
+  );
+}
+
+export default App;
